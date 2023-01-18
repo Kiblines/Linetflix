4351cda854bef0ef09e1392d83af75c94b9cf832
Anthony Lines <linesanthony018@gmail.com>
2023-01-18 23:42:46 +0100

    Quentin je fais n'imp

--- Netflix lines/src/components/Navbar.jsx
+++ Netflix lines/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
     <nav>
       <a href="#home">Home</a>
-      <a href="#home">Film</a>
-      <a href="#home">Series</a>
-      <a href="#home">Contact</a>
+      <a href="#about">About</a>
+      <a href="#services">Services</a>
+      <a href="#contact">Contact</a>
     </nav>
   );
 };
